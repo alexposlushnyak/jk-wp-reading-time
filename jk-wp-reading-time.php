b29c1e8518b8adcad16d84dd940ecf1501a42a20
Alexander Poslushnyak <fr4nk.horr1gan@gmail.com>
2021-02-04 01:21:55 +0200

    Added get_reading_time method to the reading time

--- jk-wp-reading-time.php
+++ jk-wp-reading-time.php
@@ -3,10 +3,28 @@
 class jk_wp_reading_time
 {
 
-    public function init()
+    public $divisor = 200;
+
+    public function get_reading_time($content)
     {
 
+        $word_count = str_word_count(strip_tags($content));
 
+        $readingtime = ceil($word_count / $this->divisor);
+
+        if ($readingtime == 1) :
+
+            $timer = esc_html__(' minute', '@@textdomain');
+
+        else :
+
+            $timer = esc_html__(' minutes', '@@textdomain');
+
+        endif;
+
+        $totalreadingtime = $readingtime . $timer;
+
+        return $totalreadingtime;
 
     }
 
